56e71c19c2d416a22f4815731680df7effd6b5f2
maguzmana <ma.guzmana@duocuc.cl>
2024-10-02 04:07:02 -0300

    Primer commit

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,7 @@ import db
 app = Flask(__name__)
 
 # Configuración de OpenAI API
+openai.api_key = "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"
 
 @app.route('/ask', methods=['POST'])
 def ask():
